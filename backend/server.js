fff3f39136db14b399b73683a7a0bc0e4b4e134a
IngridTorres <ingridtorres201207@gmail.com>
2026-08-10 22:41:48 -0300

    backend

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const pool = require('./database/connection');
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Servidor del CSyC funcionando');
+});
+
+app.get('/test-db', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json({
+            mensaje: 'Conexión con PostgreSQL exitosa',
+            fecha: result.rows[0].now
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            mensaje: 'Error al conectar con PostgreSQL'
+        });
+    }
+});
+
+app.get('/socios', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM socios');
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            mensaje: 'Error al obtener los socios'
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
